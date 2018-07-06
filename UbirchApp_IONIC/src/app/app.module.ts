7cfae656f16f29476344fc62f02e910fa0c94a8a
Victor <victor@cameoDevs-Mini.fritz.box>
2018-07-06 11:57:06 +0200

    Test Bluetooth

--- UbirchApp_IONIC/src/app/app.module.ts
+++ UbirchApp_IONIC/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { BLE } from '@ionic-native/ble';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { DetailPage } from '../pages/detail/detail';
+import { FormatTemperaturePipe } from '../pipes/format-temperature/format-temperature';
+import { FormatTemperatureCPipe } from '../pipes/format-temperature-c/format-temperature-c';
+import { FormatTemperatureFPipe } from '../pipes/format-temperature-f/format-temperature-f';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    DetailPage,
+    FormatTemperaturePipe,
+    FormatTemperatureCPipe,
+    FormatTemperatureFPipe
   ],
   imports: [
     BrowserModule,
@@ -19,12 +28,14 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    DetailPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BLE
   ]
 })
 export class AppModule {}
