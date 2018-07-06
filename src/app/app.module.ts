dec7d488dbb1e8c0a50280dd72a5df90a3b9ad23
Victor <victor@cameoDevs-Mini.fritz.box>
2018-07-06 15:17:18 +0200

    blank

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,14 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { BLE } from '@ionic-native/ble';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { DetailPage } from '../pages/detail/detail';
-import { FormatTemperaturePipe } from '../pipes/format-temperature/format-temperature';
-import { FormatTemperatureCPipe } from '../pipes/format-temperature-c/format-temperature-c';
-import { FormatTemperatureFPipe } from '../pipes/format-temperature-f/format-temperature-f';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    DetailPage,
-    FormatTemperaturePipe,
-    FormatTemperatureCPipe,
-    FormatTemperatureFPipe
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -28,14 +19,12 @@ import { FormatTemperatureFPipe } from '../pipes/format-temperature-f/format-tem
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    DetailPage
+    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    BLE
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
