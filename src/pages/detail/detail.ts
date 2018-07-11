cf5bbe0070251254a3d67468f19d9a8111e0988c
vpatrin <victor.patrin150@gmail.com>
2018-07-11 17:34:43 +0200

    attempt to fix reading

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -123,7 +123,7 @@ export class DetailPage {
 
    ReadSignature() {
       this.ble.read(this.peripheral.id, HANDSHAKE_SERVICE, PUBLIC_KEY_CHARACTERISTIC).then(
-            data => this.showAlert('Success !', 'Characterisctic = '+ this.bin2String(data)),
+            data => this.showAlert('Success !', 'Characterisctic = '+ JSON.stringify(data)),
             () => this.showAlert('Unexpected Error', 'Failed to read signature')
       )
    }
